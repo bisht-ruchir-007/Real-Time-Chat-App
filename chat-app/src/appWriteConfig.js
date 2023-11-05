108763ff0dc24d72bef47a55d80b0b5b23d7615c
bisht-ruchir-007 <ruchircr7@gmail.com>
2023-11-05 19:31:54 +0530

    fetching messages & adding basic css

---
+++ chat-app/src/appWriteConfig.js
@@ -0,0 +1,15 @@
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID = "654738ec584aa6a2d059";
+export const DATABASE_ID = "65473dccf1fdb9fb0626";
+export const COLLECTION_ID_MESSAGES = "65473ddaddc30e7c00ec";
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APP_END_POINT)
+  .setProject(import.meta.env.VITE_APP_PROJECT_ID);
+
+export const databases = new Databases(client);
+
+export default client;
--- chat-app/src/pages/Room.jsx
+++ chat-app/src/pages/Room.jsx
@@ -1,7 +1,40 @@
-const Room = () => {
-  return (
-    <div>Room</div>
-  )
-}
+import { useState, useEffect } from "react";
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from "../appWriteConfig";
 
-export default Room
+const Room = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+    setMessages(response.documents);
+  };
+  return (
+    <main className="container">
+      <div className="room--container">
+        {messages.map((message) => (
+          <div key={message.$id} className="message--wrapper">
+            <div className="message--header">
+              <small className="message-timestamp">{message.$createdAt}</small> 
+            </div>
+            <div className="message--body">
+              <span>{message.body}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Room;
