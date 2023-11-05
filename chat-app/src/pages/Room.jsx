cd60566c50257d70953e545785b85ad6b4debff9
bisht-ruchir-007 <ruchircr7@gmail.com>
2023-11-05 19:51:43 +0530

    create & sort messages

--- chat-app/src/pages/Room.jsx
+++ chat-app/src/pages/Room.jsx
@@ -5,33 +5,78 @@ import {
   COLLECTION_ID_MESSAGES,
 } from "../appWriteConfig";
 
+import { ID, Query } from "appwrite";
+
 const Room = () => {
   const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState("");
 
   useEffect(() => {
     getMessages();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let payload = {
+      body: messageBody,
+    };
+
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    );
+
+    //console.log("Created!",response);
+    setMessages((prevState) => [response, ...messages]);
+    setMessageBody("");
+  };
+
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
-      COLLECTION_ID_MESSAGES
+      COLLECTION_ID_MESSAGES,
+      [Query.orderDesc("$createdAt")]
     );
     setMessages(response.documents);
   };
+
   return (
     <main className="container">
       <div className="room--container">
-        {messages.map((message) => (
-          <div key={message.$id} className="message--wrapper">
-            <div className="message--header">
-              <small className="message-timestamp">{message.$createdAt}</small> 
-            </div>
-            <div className="message--body">
-              <span>{message.body}</span>
-            </div>
+        <form id="message--form" onSubmit={handleSubmit}>
+          <div>
+            <textarea
+              required
+              maxLength="250"
+              placeholder="Say something..."
+              onChange={(e) => {
+                setMessageBody(e.target.value);
+              }}
+              value={messageBody}
+            ></textarea>
           </div>
-        ))}
+
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="send" />
+          </div>
+        </form>
+        <div>
+          {messages.map((message) => (
+            <div key={message.$id} className="message--wrapper">
+              <div className="message--header">
+                <small className="message-timestamp">
+                  {message.$createdAt}
+                </small>
+              </div>
+              <div className="message--body">
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
